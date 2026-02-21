7d37f19e4dba2269c6aa06190660ee53aea90726
Pratyush-bruce-Gupta <pratyushgupta04@gmail.com>
2026-02-21 15:35:52 +0530

    more ui stuff

--- TrafficAdmin/Form1.Designer.cs
+++ TrafficAdmin/Form1.Designer.cs
@@ -32,12 +32,30 @@
             panel1 = new Panel();
             pictureBox1 = new PictureBox();
             panel2 = new Panel();
+            label1 = new Label();
+            panel3 = new Panel();
+            label3 = new Label();
+            label2 = new Label();
+            panel4 = new Panel();
+            label4 = new Label();
+            label5 = new Label();
+            panel5 = new Panel();
+            label6 = new Label();
+            label7 = new Label();
+            formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            formsPlot2 = new ScottPlot.WinForms.FormsPlot();
+            button1 = new Button();
+            panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(18, 71, 127);
+            panel1.Controls.Add(button1);
             panel1.Location = new Point(-9, 96);
             panel1.Name = "panel1";
             panel1.Size = new Size(354, 609);
@@ -48,7 +66,7 @@
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(1, -2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(351, 92);
+            pictureBox1.Size = new Size(344, 92);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -62,19 +80,173 @@
             panel2.Size = new Size(344, 10);
             panel2.TabIndex = 1;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(396, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(199, 38);
+            label1.TabIndex = 2;
+            label1.Text = "Top Summary";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(18, 71, 127);
+            panel3.BorderStyle = BorderStyle.Fixed3D;
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(label2);
+            panel3.ForeColor = Color.White;
+            panel3.Location = new Point(374, 96);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(332, 201);
+            panel3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(30, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(226, 81);
+            label3.TabIndex = 1;
+            label3.Text = "84,569";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(15, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(241, 31);
+            label2.TabIndex = 0;
+            label2.Text = "Total Violation Today";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.White;
+            panel4.BorderStyle = BorderStyle.Fixed3D;
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(label5);
+            panel4.ForeColor = Color.White;
+            panel4.Location = new Point(725, 96);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(338, 201);
+            panel4.TabIndex = 4;
+            panel4.Paint += panel4_Paint;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(47, 78);
+            label4.Name = "label4";
+            label4.Size = new Size(242, 81);
+            label4.TabIndex = 1;
+            label4.Text = "1.3 lacs";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(15, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(165, 31);
+            label5.TabIndex = 0;
+            label5.Text = "Fine Collected";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.White;
+            panel5.BorderStyle = BorderStyle.Fixed3D;
+            panel5.Controls.Add(label6);
+            panel5.Controls.Add(label7);
+            panel5.ForeColor = Color.White;
+            panel5.Location = new Point(1091, 96);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(337, 201);
+            panel5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(44, 78);
+            label6.Name = "label6";
+            label6.Size = new Size(242, 81);
+            label6.TabIndex = 1;
+            label6.Text = "0.5 lacs";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(15, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(108, 31);
+            label7.TabIndex = 0;
+            label7.Text = "Fine Due";
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.DisplayScale = 1.25F;
+            formsPlot1.Location = new Point(374, 367);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(513, 264);
+            formsPlot1.TabIndex = 6;
+            formsPlot1.Load += formsPlot1_Load;
+            // 
+            // formsPlot2
+            // 
+            formsPlot2.DisplayScale = 1.25F;
+            formsPlot2.Location = new Point(937, 367);
+            formsPlot2.Name = "formsPlot2";
+            formsPlot2.Size = new Size(491, 264);
+            formsPlot2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(42, 64);
+            button1.Name = "button1";
+            button1.Size = new Size(312, 39);
+            button1.TabIndex = 0;
+            button1.Text = "Database";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Window;
             ClientSize = new Size(1455, 704);
+            Controls.Add(formsPlot2);
+            Controls.Add(formsPlot1);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(label1);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -82,5 +254,18 @@
         private Panel panel1;
         private PictureBox pictureBox1;
         private Panel panel2;
+        private Label label1;
+        private Panel panel3;
+        private Label label2;
+        private Label label3;
+        private Panel panel4;
+        private Label label4;
+        private Label label5;
+        private Panel panel5;
+        private Label label6;
+        private Label label7;
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private ScottPlot.WinForms.FormsPlot formsPlot2;
+        private Button button1;
     }
 }
--- TrafficAdmin/Form1.cs
+++ TrafficAdmin/Form1.cs
@@ -1,3 +1,7 @@
+using MathNet.Numerics;
+using ScottPlot;
+using System.Drawing;
+
 namespace TrafficAdmin
 {
     public partial class Form1 : Form
@@ -8,6 +12,90 @@ namespace TrafficAdmin
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Top 5 cities and example traffic violation counts
+            string[] cities = { "Pune", "Chandigarh", "New Delhi", "Mumbai", "Bengaluru" };
+            double[] counts = { 1987000, 985000, 1165213, 62000, 0 };
+
+            // Use cities as “bins” just like histogram bins
+            // we create arrays of “bins” that match city indices
+            double[] bins = { 0, 1, 2, 3, 4 };
+
+            // Plot as bars
+            var barPlot = formsPlot1.Plot.Add.Bars(bins, counts);
+
+            // Optional: make bars slightly narrower (not required)
+            foreach (var bar in barPlot.Bars)
+                bar.Size = 0.8;
+
+            // Apply labels by manually making tick generator
+            ScottPlot.Tick[] ticks = new ScottPlot.Tick[cities.Length];
+            for (int i = 0; i < cities.Length; i++)
+                ticks[i] = new ScottPlot.Tick(position: i, label: cities[i]);
+
+            // Assign manual tick generator to bottom axis
+            formsPlot1.Plot.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.NumericManual(ticks);
+
+            // Optional: hide default tick lines
+            formsPlot1.Plot.Axes.Bottom.MajorTickStyle.Length = 0;
+
+            // Add labels
+            formsPlot1.Plot.YLabel("Traffic Violations");
+            formsPlot1.Plot.XLabel("City");
+            formsPlot1.Plot.Title("Top 5 Cities Traffic Violations");
+
+            // Optional: reduce margin so labels show
+            formsPlot1.Plot.Axes.Margins(bottom: 0);
+
+            // Refresh display
+            formsPlot1.Refresh();
+
+            //
+
+            // Example time of day vs violations
+            // X = hours (0 to 23), Y = violation counts
+            double[] xs = { 3, 6, 9, 12, 15, 18, 21 };
+            double[] ys = { 50, 120, 300, 250, 400, 320, 200 };
+
+            // 1) plot raw data as points
+            var scatter = formsPlot2.Plot.Add.Scatter(xs, ys);
+            scatter.MarkerSize = 7;
+            scatter.LineWidth = 0;
+
+            // 2) do a curve fit (linear or polynomial fit)
+            (double a, double b) = Fit.Line(xs, ys);
+
+            // Fit function
+            double f(double x) => a + b * x;
+
+            // compute fitted y values
+            double[] ysFit = xs.Select(f).ToArray();
+
+            // 3) plot fitted curve
+            var line = formsPlot2.Plot.Add.ScatterLine(xs, ysFit);
+            line.LineWidth = 2;
+            line.LinePattern = LinePattern.Dashed;
+
+            // labels
+            formsPlot2.Plot.Title("Traffic Violations vs Time of Day");
+            formsPlot2.Plot.XLabel("Hour of Day (0–23)");
+            formsPlot2.Plot.YLabel("Violations");
+
+            // refresh
+            formsPlot2.Refresh();
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
         {
 
         }
